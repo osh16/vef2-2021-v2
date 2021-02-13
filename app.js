e44f2d8c0d5fbb0f94159934c7a150968acf37b1
Oskar Hoskuldsson <osh16@hi.is>
2021-02-13 21:25:21 +0000

    heroku afram

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 //const urlencodedParser = bodyParser.urlencoded({ extended: false})
-const registration = require("./src/registration")
+const registration = require("./src/registration");
+const dotenv = require("dotenv");
 
-const host = '127.0.0.1';
-const port = 3000;
+dotenv.config();
+
+//const host = '127.0.0.1';
+//const port = 3000;
 
 const app = express();
 
@@ -17,9 +20,13 @@ app.set('view engine', 'ejs');
 
 app.use('/', registration);
 
+const {
+	PORT : port = 3000,
+} = process.env;
+
 app.listen(port,host,() => {
 	console.log(
-	`server @ http://${host}:${port}`,
+	`server @ http://localhost:${port}`,
 	);
 });
 
--- src/db.js
+++ src/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const {
   DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
 } = process.env;
 
 //const connectionString = 'postgres://oskar:oskar@localhost/v2';
@@ -13,6 +14,8 @@ if (!connectionString) {
   console.error('Vantar DATABASE_URL');
   process.exit(1);
 }
+
+//ssl
 
 const pool = new pg.Pool({ connectionString });
 
